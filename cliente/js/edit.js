856ac329e3f156923536ef611229feac237c2bf8
Agostinabufano <agostinabufano@gmail.com>
2018-12-19 10:08:38 -0300

    new version

---
+++ cliente/js/edit.js
@@ -0,0 +1,88 @@
+function name() {
+    var nameI = $("#name").val();
+    if (nameI !== "" && nameI.length <= 30) {
+        return true;
+    } else {
+        $("#name").parent().append(`<p>Es necesario ingresar un nombre</p>`);
+        return false;
+    }
+}
+
+function surname() {
+    var surnameI = $("#surname").val();
+    if (surnameI !== "" && surnameI.length <= 30) {
+        return true;
+    } else {
+        $("#surname").parent().append(`<p>Es necesario ingresar un apellido</p>`);
+        return false;
+    }
+}
+
+function telephone() {
+    var telI = $("#tel").val();
+    if (telI !== "" && /[0-9]/.test(telI)) {
+        return true;
+    } else {
+        $("#tel").parent().append(`<p>Es necesario ingresar un número de teléfono</p>`);
+        return false;
+    }
+}
+
+function mail() {
+    var emailI = $("#mail").val();
+    if (emailI !== "" && /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.([a-zA-Z]{2,4})+$/.test(emailI)) {
+        return true;
+    } else {
+        $("#mail").parent().append(`<p>Es necesario ingresar una dirección de mail</p>`);
+        return false;
+    }
+}
+
+function loadedUser() {
+    var id = searchParams();
+    $.ajax(`http://localhost:3000/api/users/${id}`).done(function (data) {
+        var datito = JSON.parse(data);
+        $('#name').val(datito.name);
+        $('#surname').val(datito.surname);
+        $('#tel').val(datito.tel);
+        $('#mail').val(datito.mail);
+    })
+}
+
+loadedUser();
+
+function saveEditUser(e) {
+    var id = searchParams();
+    var newName = $("#name").val();
+    var newSurname = $("#surname").val();
+    var newTel = $("#tel").val();
+    var newMail = $("#mail").val();
+    if (name() && surname() && telephone() && mail()) {
+        $.ajax({
+            url: `http://localhost:3000/api/users/${id}`,
+            type: 'PUT',
+            data: {
+                name: newName,
+                surname: newSurname,
+                tel: newTel,
+                mail: newMail
+            },
+            success: function (result) {
+                $("#editUser").append(`<p>Los datos han sido modificados y guardados exitosamente. MUCHAS GRACIAS.</p>`)
+            }
+        });
+    }
+}
+
+$('#save').on('click', function (e) {
+    saveEditUser(e);
+})
+
+function searchParams() {
+    var search = window.location.search;
+    // "?id=7125"
+    search = search.slice(1, search.length);
+    search = search.split("=");
+    // "[id,7125]"
+    return search[1];
+}
---
+++ servidor/routes/index.js
@@ -0,0 +1,138 @@
+var express = require('express');
+const fs = require('fs');
+var router = express.Router();
+var path = require("path");
+
+/* GET home page. */
+
+router.get('/ping', function (req, res, next) {
+  res.send('Pong');
+});
+
+// Renderización
+router.get('/users/new', function (req, res) {
+  res.sendFile(path.join(__dirname + '/../../cliente/html/new.html'));
+});
+
+router.get('/users', function (req, res) {
+  res.sendFile(path.join(__dirname + '/../../cliente/html/index.html'));
+});
+
+router.get('/users/edit', function (req, res) {
+  res.sendFile(path.join(__dirname + '/../../cliente/html/edit.html'));
+});
+
+router.use('/css', express.static(__dirname + '/../../cliente/css'));
+
+router.use('/js', express.static(__dirname + '/../../cliente/js'));
+
+router.post('/api/user', function (req, res, next) {
+  const user = req.body;
+  if (validate(user) == true) {
+    saveData(user);
+    res.send('<div><p>Muchas gracias por registrarse</p></div>')
+  } else {
+    res.status(400).send("Es necesario que cargue correctamente sus datos");
+  }
+
+  function validate(obj) {
+    if (obj.name !== "" && obj.name.length <= 30 &&
+      obj.surname !== "" && obj.surname.length <= 30 &&
+      obj.tel !== "" && /[0-9]/.test(obj.tel) &&
+      obj.mail !== "" && /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.([a-zA-Z]{2,4})+$/.test(obj.mail)) {
+      return true;
+    } else {
+      return false
+    }
+  }
+});
+
+function id() {
+  return Math.floor((Math.random() * 1000) + 1)
+}
+
+function read() {
+  let data = fs.readFileSync("data/data.json");
+  data = JSON.parse(data);
+  return data;
+}
+
+function saveData(obj) {
+  var data = read();
+  var object = {
+    name: obj.name,
+    surname: obj.surname,
+    tel: obj.tel,
+    mail: obj.mail.toLowerCase(),
+    id: id()
+  }
+  data.arr.push(object);
+  fs.writeFileSync("data/data.json", JSON.stringify(data));
+}
+
+router.get('/api/users', function (req, res, next) {
+  var data = read();
+  var filteredUsers = [];
+  if (req.query.search == null) {
+    return res.send(JSON.stringify(data.arr))
+  } else {
+    var search = req.query.search.toLowerCase();
+    for (var i = 0; i < data.arr.length; i++) {
+      if (data.arr[i].name.toLowerCase().includes(search) || data.arr[i].surname.toLowerCase().includes(search) || data.arr[i].tel.includes(search) || data.arr[i].mail.includes(search)) {
+        filteredUsers.push(data.arr[i]);
+      }
+    }
+    res.send(JSON.stringify(filteredUsers));
+  }
+});
+
+router.get("/api/users/:id", function (req, res) {
+  var searchUserId = req.params.id;
+  var foundUser = "";
+  var data = read();
+  for (var i = 0; i < data.arr.length; i++) {
+    if (data.arr[i].id == searchUserId) {
+      foundUser = data.arr[i];
+    }
+  }
+  res.send(JSON.stringify(foundUser));
+})
+
+router.put("/api/users/:id", function (req, res) {
+  // function editUser(data) {
+  const id = req.params.id;
+  const body = req.body;
+  console.log(id, req.body);
+  console.log(req.params);
+  console.log(req.query);
+  var data = read();
+  for (var i = 0; i < data.arr.length; i++) {
+    if (data.arr[i].id == id) {
+      data.arr[i] = body;
+    }
+  }
+  fs.writeFileSync("data/data.json", JSON.stringify(data));
+  res.send("OK")
+})
+
+router.delete("/api/users/:id", function (req, res) {
+  var deleteUserId = req.params.id;
+  deleteData(deleteUserId);
+  res.send("OK");
+})
+
+function deleteData(id) {
+  var data = read();
+  var pos = -1;
+  for (var i = 0; i < data.arr.length; i++) {
+    if (data.arr[i].id == id) {
+      pos = i;
+    }
+  }
+  if (pos > -1) {
+    data.arr.splice(pos, 1);
+  }
+  fs.writeFileSync("data/data.json", JSON.stringify(data));
+}
+
+module.exports = router;
